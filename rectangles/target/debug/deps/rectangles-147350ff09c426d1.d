65f3996f1825921d222939c5ff8a25b1d5812d0d
liuboheng <lbh9311@sina.com>
2019-09-13 19:49:17 +0800

    update

---
+++ rectangles/target/debug/deps/rectangles-147350ff09c426d1.d
@@ -0,0 +1,5 @@
+/home/liuboheng/myprojects/rust-learn/rectangles/target/debug/deps/rectangles-147350ff09c426d1: src/main.rs
+
+/home/liuboheng/myprojects/rust-learn/rectangles/target/debug/deps/rectangles-147350ff09c426d1.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/liuboheng/myprojects/rust-learn/rectangles/target/debug/rectangles: /home/liuboheng/myprojects/rust-learn/rectangles/src/main.rs
