9aa474baa4c703292620b9361bb4ac7688fd1d36
liuboheng <lbh9311@sina.com>
2019-12-26 23:59:21 +0800

    update

--- ownership_demo/src/main.rs
+++ ownership_demo/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear(); // 错误!
+
+    println!("the first word is: {}", word);
 }
