2fd9aec523fd46b63f00c1781170f5cacb982844
liuboheng <lbh9311@sina.com>
2020-01-06 09:01:49 +0800

    update

--- leetcode94/src/main.rs
+++ leetcode94/src/main.rs
@@ -1,3 +1,30 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let result: Vec<i32> = Vec::new();
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
