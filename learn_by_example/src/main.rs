740ff62773ff9ff63a3d84b8d29cb20e1576b645
liuboheng <lbh9311@sina.com>
2020-01-17 21:18:15 +0800

    update

--- learn_by_example/src/main.rs
+++ learn_by_example/src/main.rs
@@ -1,3 +1,57 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x:{}, y:{}", self.x, self.y)
+    }
+}
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> 
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{} of {:b} people know binary, the other half don't", 1, 2);
+    println!("{number:>width$}", number = 2, width = 6);
+    println!("{number:>0width$}", number = 2, width = 6);
+
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    println!("{}", vec![1,2]);
 }
