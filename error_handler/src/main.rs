caea5698ecffc7e1756ecb6ad0b15931611bd1a7
liuboheng <lbh9311@sina.com>
2020-01-28 21:45:21 +0800

    update

---
+++ error_handler/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let a = Some(2).and_then(|num| num+1);
+    
+}
---
+++ lists_demo/src/third.rs
@@ -0,0 +1,16 @@
+pub struct List<T> {
+  head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+  elem: T,
+  next: Link<T>,
+}
+
+impl<T> List<T> {
+  fn new() -> List<T> {
+    List { head: None }
+  }
+}
