e8a7a5bae949eaf1efeea6fa2bbe13fa6894d128
liuboheng <lbh9311@sina.com>
2020-01-28 23:29:32 +0800

    leetcode20

--- leetcode20/src/lib.rs
+++ leetcode20/src/lib.rs
@@ -1,23 +1,58 @@
+pub struct Solution();
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack: Vec<char> = vec![];
+        let mut map = HashMap::with_capacity(3);
+        map.insert(')', '(');
+        map.insert(']', '[');
+        map.insert('}', '{');
 
-pub fn add_two(a: i32) -> i32 {
-    internal_adder(a, 2)
-}
+        for c in s.chars() {
+            if ['(', '[', '{'].contains(&c) {
+                stack.push(c)
+            } else {
+                let need = map.get(&c);
+                let last = stack.pop();
+                if need.unwrap() != &last.unwrap_or_default() {
+                    return false;
+                }
+            }
+        }
+        stack.is_empty()
+    }
 
-fn internal_adder(a: i32, b: i32) -> i32 {
-    a + b
-}
+    pub fn is_valid2(s: String) -> bool {
+        let mut stack = Vec::new();
 
+        for c in s.chars() {
+            stack.push(match c {
+                '(' => ')',
+                '[' => ']',
+                '{' => '}',
+                _ => {
+                    if Some(c) == stack.pop() {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+            })
+        }
 
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, internal_adder(2, 2));
+        stack.is_empty()
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::Solution;
 
+    #[test]
+    fn is_valid() {
+        assert_eq!(Solution::is_valid(String::from("()")), true);
+        assert_eq!(Solution::is_valid(String::from("()[]{}")), true);
+        assert_eq!(Solution::is_valid(String::from("(]")), false);
+        assert_eq!(Solution::is_valid(String::from("([)]")), false);
+        assert_eq!(Solution::is_valid(String::from("{[]}")), true);
+    }
+}
